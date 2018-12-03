87e2319134f75bf0f049f26291c4a4632638be20
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2018-12-03 10:17:18 +0000

    add sensible comments and descriptions

--- Code/extract_Phoneme.m
+++ Code/extract_Phoneme.m
@@ -37,7 +37,9 @@ end
 %%
 if isa(dataIn,'struct')
     % 1) a struct of the form EPG_Parameters
-    
+    % This is useful when the EPG_parameters have been previously
+    % calculated, so it is possible to extract many phonemes without having
+    % to read every time
     EPG_parameters = dataIn;
     % find locations of a given phoneme.
     placePhoneme            = find(sum(strcmp(EPG_parameters.Phonemes,desiredPhoneme),2));
@@ -46,44 +48,51 @@ if isa(dataIn,'struct')
         for k3 = 1:numel(placePhoneme)
             initTime                = EPG_parameters.Phonemes{placePhoneme(k3),1};
             finTime                 = EPG_parameters.Phonemes{placePhoneme(k3),2};
-            timePhoneme             = find((EPG_parameters.EPGtimeVector>initTime).*(EPG_parameters.EPGtimeVector<finTime));
-            
+            timePhoneme             = find((EPG_parameters.EPGtimeVector>initTime).*(EPG_parameters.EPGtimeVector<finTime));            
+            % As a phoneme can occur several times in a phrase, the sum
+            % will depend on how many times it occurs plus how long each
+            % occurrence is
             avPhoneme2               = sum(EPG_parameters.Palatogram(:,:,:,timePhoneme),4);
             avPhoneme(:,:,numEventsPhoneme+k3) = avPhoneme2;            
         end
-        %avPhoneme                   = avPhoneme + avPhoneme2;
-        %imagesc(avPhoneme)
-        %title(strcat('Phoneme:',32,EPG_parameters.Phonemes{placePhoneme(1),3}),'fontsize',20)
     end
     
 elseif isfile(dataIn)
     if strcmp(dataIn(end-2:end),'mat')
-        % 2.a) where there is one .mat file with everything calculated
+        % 2.a) where there is one .mat file with everything calculated load
+        % the file and call again with the EPG_parameters, it will go
+        % directly to 1)
         load (dataIn,'EPG_parameters');
         avPhoneme               = extract_Phoneme_EPG(EPG_parameters,desiredPhoneme,avPhoneme);
     else
-        % 2.b) a file location where there are 3 files .epg, .wav, .TextGrid
-        % find the last . to determine the extensions
+        % 2.b) a file location where there are 3 files .epg, .wav,
+        % .TextGrid It will CALCULATE the EPG_parameters and then call
+        % again with the EPG_parameters, it will go directly to 1) Notice
+        % that this is slower than reading a previously calculated
+        % EPG_parameters
         
+        
+        % find the last . to determine the extensions
         findStops               = strfind(dataIn,'.');
         baseName                = dataIn(1:findStops(end));
+        % calculate all the names of the three required files
         currentWAV_File         = strcat(baseName,'wav');
         currentTEXTGRID_File    = strcat(baseName,'TextGrid');
         currentEPG_File         = strcat(baseName,'epg');
+        % read the files and calculate the parameters
         EPG_parameters          = readAudioFile (currentWAV_File);
         EPG_parameters          = interpretLabelledPhonemes(currentTEXTGRID_File,EPG_parameters);
         EPG_parameters          = readPalatogram(currentEPG_File,EPG_parameters);
-        
+        % optionally you phonation, but for the time being it is not
+        % necessary
         %EPG_parameters.Phonemes = add_Phonation_to_Phonemes(EPG_parameters.Phonemes);
-        
+        % Call the function again
         avPhoneme               = extract_Phoneme_EPG(EPG_parameters,desiredPhoneme,avPhoneme);
-        
     end
-    
     
 elseif isfolder(dataIn)
     % 3) a folder location with a series of files as above
-        
+    % Will iterate over all the files    
     dir4_EPG            = dir(strcat(dataIn,filesep,'*.epg'));
     dir4_WAV            = dir(strcat(dataIn,filesep,'*.wav'));
     dir4_TextGrid       = dir(strcat(dataIn,filesep,'*.TextGrid'));
@@ -110,28 +119,14 @@ elseif isfolder(dataIn)
             EPG_parameters          = interpretLabelledPhonemes(currentTEXTGRID_File,EPG_parameters);
             EPG_parameters          = readPalatogram(currentEPG_File,EPG_parameters);
         end
-%%
-        save(currentMAT_File,'EPG_parameters')
-        avPhoneme               = extract_Phoneme_EPG(EPG_parameters,desiredPhoneme,avPhoneme);
-    
-        %imagesc(avPhoneme)
-        %drawnow;
         
- 
+        % optional to save the EPG_parameters as a *.mat file, useful later
+        % on
+        %save(currentMAT_File,'EPG_parameters')
+        avPhoneme               = extract_Phoneme_EPG(EPG_parameters,desiredPhoneme,avPhoneme);        
     end
-    
-    
-    
-    
 end
 
-
-
-
+% Remove any level of avPhonemes with just zeros. It may have come from the
+% initialisation
 avPhoneme(:,:,find([sum(sum(avPhoneme))]==0))=[];
-
-%%
-
-
-
-
